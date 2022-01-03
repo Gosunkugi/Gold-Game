141446375f96771682eb8aa3ef26a0a536086de7
jo-kos <56891446+jo-kos@users.noreply.github.com>
2022-01-03 12:22:13 -0800

    Shop UI
    Added labels for the shop

--- Shop.cs
+++ Shop.cs
@@ -6,6 +6,8 @@ using Engine.Factories;
 
 public class Shop : Node2D
 {
+	int survivalPoints = 0;
+
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
@@ -21,12 +23,18 @@ public class Shop : Node2D
 //  {
 //      
 //  }
+	
 
 	//BUY SHIT WITH MONEY
 	private void _on_BuyButton_pressed()
 	{
 		var greeting = this.GetNode<Label>("GreetingLabel");
 		greeting.Text = "what would you like to buy?";
+
+		survivalPoints = survivalPoints + 1;
+		var survivalLabel = this.GetNode<Label>("SurvivaLLabel/SurvivalPtLabel");
+		survivalLabel.Text = survivalPoints.ToString();
+
 	}
 
 	//SELL NUGGETS FOR MONEY
@@ -34,6 +42,8 @@ public class Shop : Node2D
 	{
 		var greeting = this.GetNode<Label>("GreetingLabel");
 		greeting.Text = "what would you like to sell?";
+
+
 	}
 
 	//GO BACK TO MAP MENU
